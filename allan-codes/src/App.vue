3ab652db6ad56a40ce1115b1464ac32a8055a493
Allan McKernan <meagusxlr@hotmail.com>
2019-02-05 22:29:07 +0000

    Converting over the a VueJS site

---
+++ allan-codes/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <home/>
+  </div>
+</template>
+
+<script>
+import home from './components/pages/home.vue';
+
+export default {
+  name: 'app',
+  components: {
+    home
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ allan-codes/src/components/includes/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <a href="/">[AllanCodes]</a>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+  name: 'headerComponent'
+}
+</script>
